844a66b94751cdfc7a8f42470a7db27ebfb07616
renatof4rias <renatof4rias@gmail.com>
2024-01-06 12:27:34 -0300

    Updated GameService

--- src/main/java/com/softelse/services/GameService.java
+++ src/main/java/com/softelse/services/GameService.java
@@ -18,6 +18,7 @@ public class GameService {
 	public List<GameMinDTO> findAll() {
 		List<Game> result = gameRepository.findAll();
 		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
+
 		return dto;
 	}
 
