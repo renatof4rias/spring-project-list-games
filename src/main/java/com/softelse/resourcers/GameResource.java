28930cee92feec0f03a1efb46fcfdb398f073c16
renatof4rias <renatof4rias@gmail.com>
2024-01-08 19:43:19 -0300

    GET game by id

--- src/main/java/com/softelse/resourcers/GameResource.java
+++ src/main/java/com/softelse/resourcers/GameResource.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.softelse.dto.GameDTO;
 import com.softelse.dto.GameMinDTO;
 import com.softelse.services.GameService;
 
@@ -23,4 +25,11 @@ public class GameResource {
 		List<GameMinDTO> result = gameService.findAll();
 		return ResponseEntity.ok().body(result);
 	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<GameDTO> findById(@PathVariable Long id){
+		GameDTO result = gameService.findById(id);
+		return ResponseEntity.ok().body(result);
+	}
+	
 }
