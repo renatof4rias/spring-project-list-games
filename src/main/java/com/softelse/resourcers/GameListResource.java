a6586919ba9343d3fad53157da4d90832aa91100
renatof4rias <renatof4rias@gmail.com>
2024-01-09 21:27:11 -0300

    POST List Games

--- src/main/java/com/softelse/resourcers/GameListResource.java
+++ src/main/java/com/softelse/resourcers/GameListResource.java
@@ -6,11 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.softelse.dto.GameListDTO;
 import com.softelse.dto.GameMinDTO;
+import com.softelse.dto.ReplacementDTO;
 import com.softelse.services.GameListService;
 import com.softelse.services.GameService;
 
@@ -36,4 +39,9 @@ public class GameListResource {
 		return ResponseEntity.ok().body(result);
 	}
 
+	@PostMapping(value = "/{listId}/games")
+	public void move(@PathVariable Long listId, @RequestBody ReplacementDTO body) {
+		gameListService.move(listId, body.getSourceIndex(), body.getDestinationIndex());
+	}
+	
 }
