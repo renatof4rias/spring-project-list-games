16bc6dfd2b58afe06b7c2c1e1d118a7bd91c49a1
renatof4rias <renatof4rias@gmail.com>
2024-01-09 21:14:42 -0300

    Move Id to new Id

--- src/main/java/com/softelse/services/GameListService.java
+++ src/main/java/com/softelse/services/GameListService.java
@@ -1,5 +1,6 @@
 package com.softelse.services;
 
+import java.util.Iterator;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +9,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.softelse.dto.GameListDTO;
 import com.softelse.entities.GameList;
+import com.softelse.projections.GameMinProjection;
 import com.softelse.repositories.GameListRepository;
+import com.softelse.repositories.GameRepository;
 
 @Service
 public class GameListService {
@@ -16,10 +19,30 @@ public class GameListService {
 	@Autowired
 	private GameListRepository gameListRepository;
 
+	@Autowired
+	private GameRepository gameRepository;
+	
 	@Transactional(readOnly = true)
 	public List<GameListDTO> findAll() {
 		List<GameList> result = gameListRepository.findAll();
 		return result.stream().map(x -> new GameListDTO(x)).toList();
 	}
-
+	@Transactional
+	public void move(Long listId, int sourceIndex, int destinationIndex) {
+		
+		List<GameMinProjection> list = gameRepository.searchByList(listId);
+		
+		GameMinProjection obj = list.remove(sourceIndex);
+		list.add(destinationIndex, obj);
+	
+		int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+		int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+		
+		for (int i = min; i<= max; i++) {
+			gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+		}
+		
+		
+	}
+	
 }
